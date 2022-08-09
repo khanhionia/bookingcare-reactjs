65c2374bbd5829a94c60b11b8d015f0ee74d134d
khanhionia <khanhioniakira@gmail.com>
2022-08-09 10:19:58 +0700

    before fixbug

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -4,6 +4,7 @@ import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
 import './Login.scss';
 import { FormattedMessage } from 'react-intl';
+import { handleLoginApi } from '../../services/userService';
 
 
 class Login extends Component {
@@ -26,9 +27,14 @@ class Login extends Component {
             password: e.target.value
         })
     }
-    handleLogin = () => {
+    handleLogin = async () => {
         console.log('username: ', this.state.username);
         console.log('pww: ', this.state.password);
+        try {
+            await handleLoginApi(this.state.username, this.state.password);
+        } catch (e) {
+            console.log(e);
+        }
     }
     handleShowHidePassword = () => {
         // let isS = isShowPassword;
